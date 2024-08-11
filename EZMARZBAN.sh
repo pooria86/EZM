aae45fd87ef8cc965a026e2e62a37bb37e435e80
pooria86 <pooria.jahanara.1386@gmail.com>
2024-08-12 03:15:37 +0330

    Update EZMARZBAN.sh

--- EZMARZBAN.sh
+++ EZMARZBAN.sh
@@ -3,5 +3,7 @@
 # اجرای apt update و apt upgrade
 apt update && apt upgrade -y
 
+sleep 10
+
 # اجرای اسکریپت Marzban
 sudo bash -c "$(curl -sL https://github.com/Gozargah/Marzban-scripts/raw/master/marzban.sh)" install
